9415bb67361ef6cfee9be9f653c499bc29619dcc
AdamB <adam.blinzler@gmail.com>
2021-09-27 17:20:51 -0700

    unit tests

--- d1/d1-2.py
+++ d1/d1-2.py
@@ -1,19 +1,26 @@
-found = False
-with open( "input.txt" ) as fl:
-    nums = [ int(i) for i in fl.readlines() ]
+def example():
+    return [1721, 979, 366, 299, 675, 1456]
+    
+def input_data():
+    with open( "input.txt" ) as fl:
+        nums = [ int(i) for i in fl.readlines() ]
+    return nums
+    
+    
+def find_it(nums):
+    for idx in range(len(nums)):
+        for idy in range(len(nums))[idx+1:]:
+            for idz in range(len(nums))[idy+1:]:
+                if (nums[idx] + nums[idy] + nums[idz] == 2020):
+                    print("found it!")
+                    found = True
+                    print(nums[idx],",", nums[idy], ",", nums[idz])
+                    print(nums[idx] * nums[idy] * nums[idz] )
+                    return nums[idx] * nums[idy] * nums[idz]
 
-for idx in range(len(nums)):
-    for idy, _ in enumerate(nums[idx+1:]):
-        for idz, _ in enumerate(nums[idy+1:]):
-            if (nums[idx] + nums[idy] + nums[idz] == 2020):
-                print("found it!")
-                found = True
-                print(nums[idx],",", nums[idy], ",", nums[idz])
-                print(nums[idx] * nums[idy] * nums[idz] )
-                break;
-        if found:
-            break;
-    if found:
-        break;
+def test_example():
+    assert find_it(example()) == 241861950
+
+find_it(input_data())
 
 print ("script done")
--- d1/d1.py
+++ d1/d1.py
@@ -1,16 +1,24 @@
-found = False
-with open( "input.txt" ) as fl:
-    nums = [ int(i) for i in fl.readlines() ]
 
-for idx in range(len(nums)):
-    for idy, _ in enumerate(nums[idx+1:]):
-        if (nums[idx] + nums[idy] == 2020):
-            print("found it!")
-            found = True
-            print(nums[idx],",", nums[idy])
-            print(nums[idx] * nums[idy])
-            break;
-    if found:
-        break;
+def example():
+    return [1721, 979, 366, 299, 675, 1456]
+
+def input_data():
+    with open( "input.txt" ) as fl:
+        nums = [ int(i) for i in fl.readlines() ]
+    return nums
+
+def find_it(nums):
+    for idx in range(len(nums)):
+        for idy in range(len(nums))[idx+1:]:
+            if (nums[idx] + nums[idy] == 2020):
+                print("found it!")
+                print(nums[idx],",", nums[idy])
+                print(nums[idx] * nums[idy])
+                return nums[idx] * nums[idy]
+
+def test_example():
+    assert find_it(example()) == 514579
+
+find_it(input_data())
 
 print ("script done")
